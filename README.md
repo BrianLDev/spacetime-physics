60e43b68fefec5a46fd5198acf39dd5ae375eb55
Brian Leip <brianleip@gmail.com>
2021-07-09 21:33:28 -0700

    Created simple index page and rough structure for spacetime calculator page using bootstrap 5

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Spacetime Physics - Companion Site
 
-This is a companion site to the excellent book on Special Relativity: Spacetime Physics.  This site is intended to add interactive tools and visuals to aid understanding the nuances of Special Relativity and spacetime.
+This is a companion site to the excellent book on Special Relativity: Spacetime Physics.  it's intenet is to add interactive tools and visuals to aid understanding the nuances of Special Relativity and spacetime.
 
 ```
 Spacetime Physics, Second Edition
---
+++ js/spacetimecalc.js
@@ -0,0 +1,74 @@
+// Hide the results when page first loaded/reloaded
+document.addEventListener('DOMContentLoaded', hideResults);
+
+// Listen for submit event (note that submit "bubbles up" to parent loan-form element)
+document.getElementById('loan-form').addEventListener('submit', beginResults);
+
+
+
+// Hide results
+function hideResults() {
+  document.getElementById('results').hidden = true;
+  document.getElementById('loading').style.display = 'none';
+}
+
+// Begin Results Process
+function beginResults(e) {
+  hideResults();
+  // show loader
+  document.getElementById('loading').style.display = 'block';
+  // then calculate results after timeout
+  setTimeout(calculateResults, 500);
+
+  e.preventDefault();
+}
+
+// Calculate Results
+function calculateResults() {
+  // UI Variables
+  // TODO: ADD THESE
+
+  hideResults();
+
+  // TODO: CALCULATE RESULTS
+
+  // unhide results UI
+  document.getElementById('results').hidden = false;
+
+  // Display results
+  
+  // Validate then display results
+  if (true) { // TODO: REPLACE TRUE WITH VALIDATION
+
+  } else {
+    showError('Error: Please double check values');
+  }
+}
+
+// Show Error
+function showError(error) {
+  hideResults();
+  
+  // Get elements from UI
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+
+  // Create a div
+  const errorDiv = document.createElement('div');
+
+  // Add class
+  errorDiv.className = 'alert alert-danger';  // bootstrap classes for alert
+
+  // Create text node and append to div
+  errorDiv.appendChild(document.createTextNode(error));
+
+  // Insert error above heading
+  card.insertBefore(errorDiv, heading);
+
+  // Clear error after 3 seconds
+  setTimeout(clearError, 3000); // 3000ms = 3s
+}
+
+function clearError() {
+  document.querySelector('.alert').remove();
+}
